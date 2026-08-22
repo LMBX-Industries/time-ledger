cd2e183bdeefdbc19a3246b6baf0cc8126790583
UmbraGameDev <hexmal@gmail.com>
2026-08-22 04:56:37 -0500

    Add service worker for offline functionality
    Implement service worker for offline caching of app assets.

---
+++ sw.js
@@ -0,0 +1,46 @@
+// This is the "Offline Engine". It saves your files to the phone's hard drive.
+const CACHE_NAME = 'time-ledger-cache-v1';
+
+// The files we want to save for offline use
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// 1. Install Event: When the app is installed, save the files to cache.
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Caching app assets');
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// 2. Activate Event: Cleans up any old caches if you update the app later.
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            console.log('Clearing old cache');
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// 3. Fetch Event: When the app asks for a file, check the cache first.
+// If there is no internet, it serves the cached HTML file perfectly!
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      // Return the cached file if found, otherwise fetch from the network
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
