c3c89fb02eaa433a059ed066bfaa5ab0f883966d
UmbraGameDev <hexmal@gmail.com>
2026-09-01 18:01:49 -0500

    Update service worker cache version and assets

--- sw.js
+++ sw.js
@@ -1,24 +1,22 @@
-// This is the "Offline Engine". It saves your files to the phone's hard drive.
-const CACHE_NAME = 'time-ledger-cache-v2.6';
+const CACHE_NAME = 'time-ledger-cache-v3.4';
 
-// The files we want to save for offline use
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
-  './manifest.json'
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
-// 1. Install Event: When the app is installed, save the files to cache.
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log('Caching app assets');
+      console.log('Caching app assets v3.4');
       return cache.addAll(ASSETS_TO_CACHE);
     })
   );
 });
 
-// 2. Activate Event: Cleans up any old caches if you update the app later.
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((cacheNames) => {
@@ -34,12 +32,9 @@ self.addEventListener('activate', (event) => {
   );
 });
 
-// 3. Fetch Event: When the app asks for a file, check the cache first.
-// If there is no internet, it serves the cached HTML file perfectly!
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((cachedResponse) => {
-      // Return the cached file if found, otherwise fetch from the network
       return cachedResponse || fetch(event.request);
     })
   );
